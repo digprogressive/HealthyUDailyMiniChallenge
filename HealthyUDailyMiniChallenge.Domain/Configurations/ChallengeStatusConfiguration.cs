63552e6d5c52686f236bf96efba5bdbefc2d9f2b
Evan Lee <evanandrewlee@gmail.com>
2014-04-17 17:00:01 -0400

    Fixed Broken Merge

---
+++ HealthyUDailyMiniChallenge.Domain/Configurations/ChallengeStatusConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HealthyUDailyMiniChallenge.Domain.Entities;
+
+namespace HealthyUDailyMiniChallenge.Domain.Configurations
+{
+    class ChallengeStatusConfiguration: EntityTypeConfiguration<ChallengeStatus>
+    {
+        public ChallengeStatusConfiguration()
+        {
+            MapToStoredProcedures();
+        }
+    }
+}
